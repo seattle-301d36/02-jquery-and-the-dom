35609f2d65a023ff951881d158477523042ecce8
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-11 10:08:12 -0700

    answered ~3, modifying baseline constructor function. Functional constructor loop

--- max-brandon/scripts/article.js
+++ max-brandon/scripts/article.js
@@ -3,20 +3,26 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// This function will create a constructor object out of each blog instance.
 
 function Article (rawDataObj) {
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // Cloning the element is faster than directly targeting it. This is because modifying a clone and replacing the element in the dom is faster than pulling an element out of the dom, modifying it, and putting back into the dom. Also the clone does not relocate an element from its old location the way append does.
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
-
+  $newArticle.removeClass('template');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
@@ -44,6 +50,9 @@ rawData.sort(function(a,b) {
 for(let i = 0; i < rawData.length; i++) {
   articles.push(new Article(rawData[i]));
 }
+// rawData.forEach(function(data){
+//   articles.push(new Article(data));
+// });
 
 for(let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
